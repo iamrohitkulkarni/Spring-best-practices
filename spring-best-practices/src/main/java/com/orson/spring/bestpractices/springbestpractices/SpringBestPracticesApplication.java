a7d169901774f5605b1935a92e8a368cd2ace4a7
Rohit Kulkarni <rdk4online@gmail.com>
2018-11-17 11:47:55 +0530

    Best JPA Practices Added

---
+++ spring-best-practices/src/main/java/com/orson/spring/bestpractices/springbestpractices/SpringBestPracticesApplication.java
@@ -0,0 +1,12 @@
+package com.orson.spring.bestpractices.springbestpractices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBestPracticesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBestPracticesApplication.class, args);
+	}
+}
